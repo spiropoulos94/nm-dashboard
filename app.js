6ca8a058135f140946cc89909a1c6f3d6b55c2ed
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-08-19 13:12:23 +0300

    added functions

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-//window.onload = windowFN
+const main_content = document.getElementsByClassName("main-screen-content")[0]
 
-window.onload = () => {
-    const body = document.querySelector('body')
-    console.log(body)
+
+function welcomeScreen() {
+    main_content.innerHTML = "<h1>Welcome </h1>"
+}
+
+function getColours() {
+    fetch("https://reqres.in/api/products/")
+        .then(res => res.json())
+        .then(res => {
+            colours = res.data
+            console.log('Colors Data', colours)
+            main_content.innerHTML = "<h1>Colours</h1>"
+            colours.forEach(colour => {
+                let colorTab = document.createElement('div')
+
+            });
+        })
+}
+
+
+
+function getUsers() {
+    fetch("https://reqres.in/api/users")
+        .then(res => res.json())
+        .then(res => {
+            let users = res.data
+            console.log("User Data", users)
+            main_content.innerHTML = "<h1>Users</h1>"
+        })
 }
