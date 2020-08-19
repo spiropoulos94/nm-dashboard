5602fa22854daf7fc546b043b944a0dd343414b6
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-08-19 14:15:08 +0300

    latest commit

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-const main_content = document.getElementsByClassName("main-screen-content")[0]
+const main_content = document.getElementsByClassName("main-screen-content")[0];
 
+let mainHeading = document.createElement("h1")
 
 function welcomeScreen() {
-    main_content.innerHTML = "<h1>Welcome </h1>"
+
+    mainHeading.innerText = "Welcome"
+    main_content.appendChild(mainHeading)
+    main_content.innerHTML = "<p>fafasfafas</p>"
+    console.log(mainHeading)
 }
 
 function getColours() {
     fetch("https://reqres.in/api/products/")
-        .then(res => res.json())
-        .then(res => {
-            colours = res.data
-            console.log('Colors Data', colours)
-            main_content.innerHTML = "<h1>Colours</h1>"
-            colours.forEach(colour => {
-                let colorTab = document.createElement('div')
+        .then((res) => res.json())
+        .then((res) => {
+            colours = res.data;
+            console.log("Colors Data", colours);
+            let h1 = document.createElement('h1')
+            h1.innerText = "Colours"
+                //main_content.innerHTML = "<h1>Colours</h1>";
+            let blurbsContainer = document.createElement("div");
+            blurbsContainer.className = "blurbs-container";
+            main_content.innerHTML = ""
+            main_content.appendChild(h1)
+            main_content.appendChild(blurbsContainer);
 
+            colours.forEach((color) => {
+                let imageDiv = document.createElement("div");
+                imageDiv.className = "blurb";
+                imageDiv.innerHTML = `<h3>${color.name}</h3>
+                <p>${color.year}</p>
+                `;
+                blurbsContainer.appendChild(imageDiv);
             });
-        })
+        });
 }
-
-
 
 function getUsers() {
     fetch("https://reqres.in/api/users")
-        .then(res => res.json())
-        .then(res => {
-            let users = res.data
-            console.log("User Data", users)
-            main_content.innerHTML = "<h1>Users</h1>"
-        })
+        .then((res) => res.json())
+        .then((res) => {
+            let users = res.data;
+            console.log("User Data", users);
+            main_content.innerHTML = "<h1>Users</h1>";
+        });
 }
