15ecca3eae7b17b953a69e8d3032708f1997fa8b
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-08-19 14:34:36 +0300

    last commit before changing directory

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const main_content = document.getElementsByClassName("main-screen-content")[0];
 
-let mainHeading = document.createElement("h1")
+let mainHeading = document.getElementsByClassName("main-heading")[0];
 
 function welcomeScreen() {
-
-    mainHeading.innerText = "Welcome"
-    main_content.appendChild(mainHeading)
-    main_content.innerHTML = "<p>fafasfafas</p>"
-    console.log(mainHeading)
+    mainHeading.innerText = "Welcome";
 }
 
 function getColours() {
@@ -16,14 +12,9 @@ function getColours() {
         .then((res) => {
             colours = res.data;
             console.log("Colors Data", colours);
-            let h1 = document.createElement('h1')
-            h1.innerText = "Colours"
-                //main_content.innerHTML = "<h1>Colours</h1>";
+
             let blurbsContainer = document.createElement("div");
             blurbsContainer.className = "blurbs-container";
-            main_content.innerHTML = ""
-            main_content.appendChild(h1)
-            main_content.appendChild(blurbsContainer);
 
             colours.forEach((color) => {
                 let imageDiv = document.createElement("div");
@@ -42,6 +33,5 @@ function getUsers() {
         .then((res) => {
             let users = res.data;
             console.log("User Data", users);
-            main_content.innerHTML = "<h1>Users</h1>";
         });
 }
