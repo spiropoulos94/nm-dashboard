95d445dc326e0831a361575dffaf6b875fa1946d
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-19 21:34:44 +0300

    last commit before delete button

--- assets/app.js
+++ assets/app.js
@@ -1,73 +1,86 @@
 window.addEventListener("load", function() {
-    const data = document.getElementsByClassName("data")[0];
-    const mainHeading = document.getElementsByClassName("main-heading")[0];
-    const info = document.getElementById("info");
-    const mainHeaderContainer = document.getElementsByClassName(
-        "main-header-container"
-    )[0];
+  let data = document.getElementsByClassName("data")[0];
+  let mainHeading = document.getElementsByClassName("main-heading")[0];
+  let info = document.getElementById("info");
+  let mainHeaderContainer = document.getElementsByClassName(
+    "main-header-container"
+  )[0];
 
-    function welcomeScreen() {
-        data.innerHTML = "";
-        mainHeading.innerText = "Welcome";
-    }
+  let flexibleField = document.getElementsByClassName("flexible")[0];
+  let displayLength = document.getElementsByClassName("length")[0];
 
-    let options = document.getElementById("data-length");
+  function welcomeScreen() {
+    data.innerHTML = "";
+    mainHeading.innerText = "Welcome";
+    flexibleField.innerHTML = ``;
+    displayLength.innerHTML = ``;
+  }
 
-    document
-        .getElementsByClassName("logo-txt")[0]
-        .addEventListener("click", () => {
-            welcomeScreen();
-        });
+  let options = document.getElementById("data-length");
 
-    function getColours() {
-        fetch("https://reqres.in/api/products/")
-            .then((res) => res.json())
-            .then((res) => {
-                colours = res.data;
-                console.log(colours.length);
-                console.log("Colors Data", colours);
+  document
+    .getElementsByClassName("logo-txt")[0]
+    .addEventListener("click", () => {
+      welcomeScreen();
+    });
 
-                mainHeading.innerText = "Colours";
+  function getColours() {
+    fetch("https://reqres.in/api/products/")
+      .then((res) => res.json())
+      .then((res) => {
+        colours = res.data;
+        console.log(colours.length);
+        console.log("Colors Data", colours);
 
-                let blurbsContainer = document.createElement("div");
-                blurbsContainer.className = "blurbs-container";
-                data.innerText = "";
+        mainHeading.innerText = "Colours";
 
-                colours.forEach((color) => {
-                    let imageDiv = document.createElement("div");
-                    imageDiv.className = "blurb";
-                    imageDiv.innerHTML = `
-                <div style="background-color:${color.color};">
+        let blurbsContainer = document.createElement("div");
+        blurbsContainer.className = "blurbs-container";
+        data.innerText = "";
+
+        flexibleField.innerHTML = `<p>Items </p>`;
+        displayLength.innerHTML = `<p>${colours.length}</p>`;
+
+        colours.forEach((color) => {
+          let imageDiv = document.createElement("div");
+          imageDiv.className = "blurb";
+          imageDiv.setAttribute("style", `background-color:${color.color}`);
+          imageDiv.innerHTML = `
                 <h3 class="color-code" style="color:${color.color}" >${color.color}</h3>
-                <div class="color-info">            
-                
+                <div class="color-info">
                 <p class="color-year">${color.year}</p>
                 <p class="color-name">${color.name}</p>
                 </div>
-                </div>
                 `;
-                    data.appendChild(imageDiv);
-                });
-            });
-        mainHeaderContainer.appendChild(info);
-    }
+          data.appendChild(imageDiv);
+        });
+      });
+  }
 
-    document.getElementById("colours").addEventListener("click", () => {
-        getColours();
-    });
+  document.getElementById("colours").addEventListener("click", () => {
+    getColours();
+  });
 
-    function getUsers() {
-        fetch("https://reqres.in/api/users")
-            .then((res) => res.json())
-            .then((res) => {
-                let users = res.data;
-                console.log("User Data", users);
-            });
-        data.innerHTML = "";
-        mainHeading.innerText = "User Data";
-    }
+  function getUsers() {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((res) => {
+        let users = res.data;
+        console.log("User Data", users);
+      });
+    data.innerHTML = "";
+    mainHeading.innerText = "User Data";
+    const button = document.createElement("button");
+    button.innerHTML = "Delete";
+    flexibleField = button;
 
-    document.getElementById("users").addEventListener("click", () => {
-        getUsers();
-    });
+    console.log(flexibleField);
+
+    //flexibleField.innerHTML = `Delete`;
+    displayLength.innerHTML = ``;
+  }
+
+  document.getElementById("users").addEventListener("click", () => {
+    getUsers();
+  });
 });
