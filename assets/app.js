6e545acb561c9598b4235b0cb3f86e7962d06d81
marinakmp <m.kampisiou@netmechanics.gr>
2020-08-24 19:02:07 +0300

    TODOs and Notes added, small improvement made

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,8 @@
 // the following event listener is responsible for displaying or not the menu based on the window width
 
+// TODO remove unused variables
+// TODO Avoid selecting the same element more than once
+
 window.addEventListener("resize", function() {
     let navBar = document.querySelector("div.nav-bar");
     if (window.innerWidth < 730) {
@@ -9,9 +12,9 @@ window.addEventListener("resize", function() {
     }
 });
 
-window.addEventListener("load", function onloadFn() {
+function onloadFn() {
     //when page loads the spinner becomes hidden
-    window.removeEventListener("load", onloadFn);
+    // TODO IDs naming convention
     let spinner = document.getElementById("whole-page-spinner");
 
     spinner.style.display = "none";
@@ -19,9 +22,7 @@ window.addEventListener("load", function onloadFn() {
     let data = document.getElementsByClassName("data")[0];
     let mainHeading = document.getElementsByClassName("main-heading")[0];
     let info = document.getElementById("info");
-    let mainHeaderContainer = document.getElementsByClassName(
-        "main-header-container"
-    )[0];
+    let mainHeaderContainer = document.getElementsByClassName("main-header-container")[0];
     let flexibleField = document.querySelector(".flexible");
     let displayLength = document.querySelector(".length");
     let dataTable = document.querySelector(".data-table");
@@ -30,7 +31,8 @@ window.addEventListener("load", function onloadFn() {
 
     let burgerMenu = document.querySelector("button.ham");
 
-    burgerMenu.onclick = function() {
+    // TODO For this project either use eventListeners on events. What is the difference between the two?
+    burgerMenu.onclick = function () {
         let navBar = document.querySelector("div.nav-bar");
         navBar.classList.toggle("not-visible");
     };
@@ -73,6 +75,7 @@ window.addEventListener("load", function onloadFn() {
                     let imageDiv = document.createElement("div");
                     imageDiv.className = "blurb";
                     imageDiv.setAttribute("style", `background-color:${color.color}`);
+                    // TODO Review HTML headings order & headings use cases.
                     imageDiv.innerHTML = `
                 <h3 class="color-code" style="color:${color.color}" >${color.color}</h3>
                 <div class="color-info">
@@ -85,47 +88,40 @@ window.addEventListener("load", function onloadFn() {
             });
     }
 
-    document.getElementById("colours").addEventListener("click", getColours);
-
     // Users Screen ----------------
-    let sessionStorageData = JSON.parse(
-        window.sessionStorage.getItem("usersData")
-    );
+    let sessionStorageData = JSON.parse(window.sessionStorage.getItem("usersData"));
 
     function hydrateUsers(fetchedData) {
-        data.innerHTML = "";
-        mainHeading.innerText = "User Data";
-
         let displaySpace = document.getElementById("space");
         let displaySpaceWrapper = document.getElementById("display-wrapper");
-        displaySpace.innerHTML =
-            '<button class="delete-btn" type="button" disabled>Delete</button>';
+        let bdxample = document.createElement("div");
+        let tableResponsive = document.createElement("div");
+        let tableStringHTML = "";
+
+        data.innerHTML = "";
+        mainHeading.innerText = "User Data";
+        displaySpace.innerHTML ='<button class="delete-btn" type="button" disabled>Delete</button>';
 
         if (displaySpace.children.length > 1) {
             displaySpace.removeChild(displaySpace.lastChild);
         }
 
         displayLength.innerHTML = "";
+
         //  Converting Data to Table
-
         dataTable.classList.remove("not-visible");
-        let bdxample = document.createElement("div");
         bdxample.className = "bd-example";
-
-        let tableResponsive = document.createElement("div");
         tableResponsive.className = "table-responsive";
 
         data.appendChild(bdxample);
         bdxample.appendChild(tableResponsive);
         tableResponsive.appendChild(dataTable);
 
-        let tableStringHTML = "";
-
         fetchedData.forEach((user) => {
             tableStringHTML += `<tr>
               <td class="td user-checkbox "><input type="radio" name="radio-btn" class="delete-checkbox " id="${
                 user.id
-              }" />
+            }" />
                 <label for="radio-btn"></label> </td>
               <td class="td user-id">${user.id}</td>
               <td class="td user-last">${user.last_name}</td>
@@ -134,7 +130,7 @@ window.addEventListener("load", function onloadFn() {
               <td class="td user-avatar"> ${user.avatar.substring(
                 user.avatar.lastIndexOf("r/") + 2,
                 user.avatar.lastIndexOf("/128")
-              )}</td></tr>
+            )}</td></tr>
             `;
         });
 
@@ -143,25 +139,7 @@ window.addEventListener("load", function onloadFn() {
         let selectedRow = null;
         let userID = null;
 
-        // Deleting a user/row  -----------------------------------------------------------------
-
-        function deleteUser(selectedRow, userId) {
-            confirm(`Are you sure you want to delete user with ID ${userID} ?`);
-
-            //remove from table
-            selectedRow.parentNode.removeChild(selectedRow);
-            // console.log(userID, parseInt(selectedRow.children[1].innerText));
-
-            //remove from session storage
-            let arrayJson = JSON.parse(window.sessionStorage.getItem("usersData"));
-
-            let newArr = arrayJson.filter(
-                (entry) => parseInt(entry.id) != parseInt(userID)
-            );
-            window.sessionStorage.setItem("usersData", JSON.stringify(newArr));
-            document.querySelector(".delete-btn").setAttribute("disabled", true);
-        }
-
+        // Note if any selector you are trying to immediately use is not in the DOM then you'll have an error
         document.querySelector(".delete-btn").addEventListener("click", () => {
             deleteUser(selectedRow, userID);
         });
@@ -173,22 +151,54 @@ window.addEventListener("load", function onloadFn() {
         });
     }
 
+    // Deleting a user/row
+    // TODO Review the following function
+    function deleteUser(selectedRow, userId) {
+        confirm(`Are you sure you want to delete user with ID ${userID} ?`);
+
+        //remove from table
+        selectedRow.parentNode.removeChild(selectedRow);
+        // console.log(userID, parseInt(selectedRow.children[1].innerText));
+
+        //remove from session storage
+        let arrayJson = JSON.parse(window.sessionStorage.getItem("usersData"));
+
+        let newArr = arrayJson.filter(
+            (entry) => parseInt(entry.id) != parseInt(userID)
+        );
+
+        // Set button to disabled
+        // TODO You might need to learn more about boolean attributes
+        document.querySelector(".delete-btn").setAttribute("disabled", "disabled");
+
+        // Update storage
+        window.sessionStorage.setItem("usersData", JSON.stringify(newArr));
+    }
+
     function getUsers() {
         if (window.sessionStorage.getItem("usersData")) {
-            let sessionStorageData = JSON.parse(
-                window.sessionStorage.getItem("usersData")
-            );
+            let sessionStorageData = JSON.parse(window.sessionStorage.getItem("usersData"));
             console.log("users hydrated from local storage");
             hydrateUsers(sessionStorageData);
+
         } else {
             fetch("https://reqres.in/api/users")
                 .then((res) => res.json())
                 .then((res) => {
-                    window.sessionStorage.setItem("usersData", JSON.stringify(res.data));
                     console.log("users hydrated by fetching data");
                     hydrateUsers(res.data);
+
+                    window.sessionStorage.setItem("usersData", JSON.stringify(res.data));
                 });
         }
     }
+
+    // Views eventListeners
+    document.getElementById("colours").addEventListener("click", getColours);
     document.getElementById("users").addEventListener("click", getUsers);
-});
+
+    window.removeEventListener("load", onloadFn);
+}
+
+// TODO Code should execute when the HTML is loaded, and the DOM tree is built.
+window.addEventListener("load", onloadFn);
