b2ceb918bff063a9b42549340bd8f7716c7239de
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-08-25 09:28:05 +0300

    refixed table

--- assets/app.js
+++ assets/app.js
@@ -3,132 +3,150 @@ document.addEventListener("DOMContentLoaded", onloadFn);
 let navBar = document.querySelector("div.nav-bar");
 let deleteButton = document.querySelector(".delete-btn");
 window.addEventListener("resize", function() {
-  if (window.innerWidth < 730) {
-    navBar.className += " not-visible";
-  } else {
-    navBar.classList.remove("not-visible");
-  }
+    if (window.innerWidth < 730) {
+        navBar.className += " not-visible";
+    } else {
+        navBar.classList.remove("not-visible");
+    }
 });
 
 function onloadFn() {
-  //when page loads the spinner becomes hidden
+    //when page loads the spinner becomes hidden
 
-  let spinner = document.getElementById("wholePageSpinner");
+    let spinner = document.getElementById("wholePageSpinner");
 
-  spinner.style.display = "none";
+    document.querySelector(".data").innerHTML = "";
 
-  let data = document.querySelector(".data");
-  let mainHeading = document.querySelector(".main-heading");
+    spinner.style.display = "none";
 
-  let info = document.getElementById("info");
-  let mainHeaderContainer = document.getElementsByClassName(
-    "main-header-container"
-  )[0];
-  let flexibleField = document.querySelector(".flexible");
-  let displayLength = document.querySelector(".length");
-  let dataTable = document.querySelector(".data-table");
+    let data = document.querySelector(".data");
+    let mainHeading = document.querySelector(".main-heading");
 
-  let tableHead = document.querySelector("table-head");
+    let info = document.getElementById("info");
+    let mainHeaderContainer = document.getElementsByClassName(
+        "main-header-container"
+    )[0];
+    let flexibleField = document.querySelector(".flexible");
+    let displayLength = document.querySelector(".length");
+    let dataTable = document.querySelector(".data-table");
 
-  let burgerMenu = document.querySelector("button.ham");
+    let tableHead = document.querySelector("table-head");
 
-  // TODO For this project either use eventListeners on events. What is the difference between the two?
-  //Answer : Events overwrite each other while even listeners don't.
+    let burgerMenu = document.querySelector("button.ham");
 
-  function toggleNavbar() {
-    navBar.classList.toggle("not-visible");
-  }
+    // TODO For this project either use eventListeners on events. What is the difference between the two?
+    //Answer : Events overwrite each other while even listeners don't.
 
-  burgerMenu.addEventListener("click", toggleNavbar);
+    function toggleNavbar() {
+        navBar.classList.toggle("not-visible");
+    }
 
-  // Welcome Screen ----------------
+    burgerMenu.addEventListener("click", toggleNavbar);
 
-  function welcomeScreen() {
-    data.innerHTML = "";
-    mainHeading.innerText = "Welcome";
-    flexibleField.innerHTML = ``;
-    displayLength.innerHTML = ``;
-  }
+    // Welcome Screen ----------------
 
-  let options = document.getElementById("data-length");
+    function welcomeScreen() {
+        data.innerHTML = "";
+        mainHeading.innerText = "Welcome";
+        flexibleField.innerHTML = ``;
+        displayLength.innerHTML = ``;
+    }
 
-  document
-    .getElementsByClassName("logo-txt")[0]
-    .addEventListener("click", welcomeScreen);
+    let options = document.getElementById("data-length");
 
-  // Colours Screen ------------------
+    document
+        .getElementsByClassName("logo-txt")[0]
+        .addEventListener("click", welcomeScreen);
 
-  function getColours() {
-    fetch("https://reqres.in/api/products/")
-      .then((res) => res.json())
-      .then((res) => {
-        colours = res.data;
+    // Colours Screen ------------------
 
-        colours.reverse();
+    function getColours() {
+        fetch("https://reqres.in/api/products/")
+            .then((res) => res.json())
+            .then((res) => {
+                colours = res.data;
 
-        mainHeading.innerText = "Colours";
+                colours.reverse();
 
-        let blurbsContainer = document.createElement("div");
-        blurbsContainer.className = "blurbs-container";
-        data.innerText = "";
+                mainHeading.innerText = "Colours";
 
-        flexibleField.innerHTML = `<p>items :</p>`;
-        displayLength.innerHTML = `<p>${colours.length}</p>`;
+                let blurbsContainer = document.createElement("div");
+                blurbsContainer.className = "blurbs-container";
+                data.innerText = "";
 
-        colours.forEach((color) => {
-          let imageDiv = document.createElement("div");
-          imageDiv.className = "blurb";
-          imageDiv.setAttribute("style", `background-color:${color.color}`);
+                flexibleField.innerHTML = `<p>items :</p>`;
+                displayLength.innerHTML = `<p>${colours.length}</p>`;
 
-          imageDiv.innerHTML = `
+                colours.forEach((color) => {
+                    let imageDiv = document.createElement("div");
+                    imageDiv.className = "blurb";
+                    imageDiv.setAttribute("style", `background-color:${color.color}`);
+
+                    imageDiv.innerHTML = `
                 <p class="color-code" style="color:${color.color}" >${color.color}</p>
                 <div class="color-info">
                 <p class="color-year">${color.year}</p>
                 <p class="color-name">${color.name}</p>
                 </div>
                 `;
-          data.appendChild(imageDiv);
-        });
-      });
-  }
-
-  // Users Screen ----------------
-  let sessionStorageData = JSON.parse(
-    window.sessionStorage.getItem("usersData")
-  );
-
-  function hydrateUsers(fetchedData) {
-    let displaySpace = document.getElementById("space");
-    let displaySpaceWrapper = document.getElementById("display-wrapper");
-    // let bdxample = document.createElement("div");
-    //let tableResponsive = document.createElement("div");
-    let tableStringHTML = "";
-
-    console.log(tableStringHTML);
-
-    data.innerHTML = "";
-    mainHeading.innerText = "User Data";
-    displaySpace.innerHTML =
-      '<button class="delete-btn" type="button" disabled>Delete</button>';
-
-    let deleteButton = document.querySelector(".delete-btn");
-
-    if (displaySpace.children.length > 1) {
-      displaySpace.removeChild(displaySpace.lastChild);
+                    data.appendChild(imageDiv);
+                });
+            });
     }
 
-    displayLength.innerHTML = "";
-    console.log(displayLength);
+    // Users Screen ----------------
+    let sessionStorageData = JSON.parse(
+        window.sessionStorage.getItem("usersData")
+    );
 
-    //  Converting Data to Table
-    dataTable.classList.remove("not-visible");
+    function hydrateUsers(fetchedData) {
+        let displaySpace = document.getElementById("space");
+        let displaySpaceWrapper = document.getElementById("display-wrapper");
+        // let bdxample = document.createElement("div");
+        //let tableResponsive = document.createElement("div");
+        let tableStringHTML = "";
 
-    //data.appendChild(bdxample);
-    // bdxample.appendChild(tableResponsive);
-    // tableResponsive.appendChild(dataTable);
+        // console.log(tableStringHTML);
 
-    fetchedData.forEach((user) => {
-      tableStringHTML += `<tr>
+        data.innerHTML = `<div class="bd-example">
+        <div class="table-responsive">
+            <table id="myTable" cell-spacing="0" cell-padding="0" class="data-table">
+                <thead>
+                    <tr class="table-head">
+                        <th></th>
+                        <th>ID</th>
+                        <th>LAST NAME</th>
+                        <th>FIST NAME</th>
+                        <th>EMAIL</th>
+                        <th>AVATAR</th>
+                    </tr>
+                </thead>
+                <tbody class="tbody"></tbody>
+            </table>
+        </div>
+    </div>`;
+
+        mainHeading.innerText = "User Data";
+        displaySpace.innerHTML =
+            '<button class="delete-btn" type="button" disabled>Delete</button>';
+
+        let deleteButton = document.querySelector(".delete-btn");
+
+        if (displaySpace.children.length > 1) {
+            displaySpace.removeChild(displaySpace.lastChild);
+        }
+
+        displayLength.innerHTML = "";
+
+        //  Converting Data to Table
+        // dataTable.classList.remove("not-visible");
+
+        //data.appendChild(bdxample);
+        // bdxample.appendChild(tableResponsive);
+        // tableResponsive.appendChild(dataTable);
+
+        fetchedData.forEach((user) => {
+            tableStringHTML += `<tr>
               <td class="td user-checkbox "><input type="radio" name="radio-btn" class="delete-checkbox " id="${
                 user.id
               }" />
@@ -142,86 +160,82 @@ function onloadFn() {
                 user.avatar.lastIndexOf("/128")
               )}</td></tr>
             `;
+        });
+        document.querySelector(".tbody").innerHTML = tableStringHTML;
 
-      //console.log(document.querySelector("tbody").parentElement);
-    });
-    // document.querySelector(".tbody").innerHTML = tableStringHTML;
+        let checkboxes = document.querySelectorAll("input.delete-checkbox");
+        let selectedRow = null;
+        let userID = null;
 
-    // dataTable.querySelector("tbody").innerHTML = tableStringHTML;
+        bdexample = document.querySelector(".bd-example");
+        tableResponsive = document.querySelector(".table-responsive");
 
-    let checkboxes = document.querySelectorAll("input.delete-checkbox");
-    let selectedRow = null;
-    let userID = null;
+        // Note if any selector you are trying to immediately use is not in the DOM then you'll have an error
+        if (deleteButton) {
+            deleteButton.addEventListener("click", () => {
+                deleteUser(selectedRow, userID);
+            });
+        }
 
-    bdexample = document.querySelector(".bd-example");
-    console.log(bdexample);
-    tableResponsive = document.querySelector(".table-responsive");
-
-    // Note if any selector you are trying to immediately use is not in the DOM then you'll have an error
-    deleteButton.addEventListener("click", () => {
-      deleteUser(selectedRow, userID);
-    });
-
-    // TODO  fix the issue of not appending table on .data div !!!!!
-
-    //document.querySelector("tbody").addEventListener("change", (e) => {
-    // deleteButton.removeAttribute("disabled");
-    // selectedRow = e.target.parentNode.parentNode;
-    // userID = selectedRow.children[1].innerText;
-    // });
-  }
-
-  // Deleting a user/row
-  // TODO Review the following function
-  function deleteUser(selectedRow, id, deleteButton) {
-    userID = selectedRow.children[1].innerText;
-    confirm(`Are you sure you want to delete user with ID ${userID} ?`);
-
-    //remove from table
-    selectedRow.parentNode.removeChild(selectedRow);
-    // console.log(userID, parseInt(selectedRow.children[1].innerText));
-
-    //remove from session storage
-    let arrayJson = JSON.parse(window.sessionStorage.getItem("usersData"));
-
-    let newArr = arrayJson.filter(
-      (entry) => parseInt(entry.id) != parseInt(userID)
-    );
-
-    // Set button to disabled
-    // TODO You might need to learn more about boolean attributes
-
-    // deleteButton.setAttribute("disabled", false);
-    if (deleteButton) {
-      deleteButton.setAttribute("disabled", false);
-    }
-
-    // Update storage
-    window.sessionStorage.setItem("usersData", JSON.stringify(newArr));
-  }
-
-  function getUsers() {
-    if (window.sessionStorage.getItem("usersData")) {
-      let sessionStorageData = JSON.parse(
-        window.sessionStorage.getItem("usersData")
-      );
-      console.log("users hydrated from local storage");
-      hydrateUsers(sessionStorageData);
-    } else {
-      fetch("https://reqres.in/api/users")
-        .then((res) => res.json())
-        .then((res) => {
-          console.log("users hydrated by fetching data");
-          hydrateUsers(res.data);
-
-          window.sessionStorage.setItem("usersData", JSON.stringify(res.data));
+        document.querySelector("tbody").addEventListener("change", (e) => {
+            deleteButton.removeAttribute("disabled");
+            selectedRow = e.target.parentNode.parentNode;
+            userID = selectedRow.children[1].innerText;
         });
     }
-  }
 
-  // Views eventListeners
-  document.getElementById("colours").addEventListener("click", getColours);
-  document.getElementById("users").addEventListener("click", getUsers);
+    // Deleting a user/row
+    // TODO Review the following function
+    function deleteUser(selectedRow, userID, deleteButton) {
+        userID = selectedRow.children[1].innerText;
+        confirm(`Are you sure you want to delete user with ID ${userID} ?`);
 
-  window.removeEventListener("load", onloadFn);
+        //remove from table
+        selectedRow.parentNode.removeChild(selectedRow);
+        // console.log(userID, parseInt(selectedRow.children[1].innerText));
+
+        //remove from session storage
+        let arrayJson = JSON.parse(window.sessionStorage.getItem("usersData"));
+
+        let newArr = arrayJson.filter(
+            (entry) => parseInt(entry.id) != parseInt(userID)
+        );
+
+        // Set button to disabled
+        // TODO You might need to learn more about boolean attributes
+
+        // deleteButton.setAttribute("disabled", false);
+        if (deleteButton) {
+            console.log(deleteButton);
+            // deleteButton.setAttribute("disabled", false);
+        }
+
+        // Update storage
+        window.sessionStorage.setItem("usersData", JSON.stringify(newArr));
+    }
+
+    function getUsers() {
+        if (window.sessionStorage.getItem("usersData")) {
+            let sessionStorageData = JSON.parse(
+                window.sessionStorage.getItem("usersData")
+            );
+            console.log("users hydrated from local storage");
+            hydrateUsers(sessionStorageData);
+        } else {
+            fetch("https://reqres.in/api/users")
+                .then((res) => res.json())
+                .then((res) => {
+                    console.log("users hydrated by fetching data");
+                    hydrateUsers(res.data);
+
+                    window.sessionStorage.setItem("usersData", JSON.stringify(res.data));
+                });
+        }
+    }
+
+    // Views eventListeners
+    document.getElementById("colours").addEventListener("click", getColours);
+    document.getElementById("users").addEventListener("click", getUsers);
+
+    window.removeEventListener("load", onloadFn);
 }
